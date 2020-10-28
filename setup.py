f34de2a64baf6ccd505cc52ce3c6ea534b3a5fa6
imjoseangel <josea.munoz@gmail.com>
2020-10-28 15:32:55 +0100

    Add new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ftpbenchmark",
-    version="1.0",
+    version="1.0.1",
     packages=["ftpbenchmark"],
     url="http://github.com/imjoseangel/ftpbenchmark",
     license="MIT",
